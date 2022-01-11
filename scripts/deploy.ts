0d956afcbd780af7ecf9bcbc3af5c152c21bde3d
Gabe (Mac Mini) <sentiinex@gmail.com>
2022-01-11 15:53:46 -0800

    Add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying with account:', deployer.address);
 
+  const blockLoggerFactory = await ethers.getContractFactory('BlockLogger');
+  const blockLogger = await blockLoggerFactory.deploy();
+  await blockLogger.deployed();
+  console.log('Block Logger address:', blockLogger.address);
+
+  const [number, timestamp] = await blockLogger.blockInformation();
+  console.log('Block number:', number);
+  console.log('Block timestamp:', timestamp);
 }
 
 main().catch((error) => {
